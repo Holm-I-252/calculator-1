e998c43b25d2a555e60d56af1c7e3daf9481e1c8
Ian Holm <ianholm252@gmail.com>
2021-07-26 15:18:10 -0600

    Finished Calculator project with exponents and remainders added

--- calc.js
+++ calc.js
@@ -14,8 +14,32 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	//Calculates numbers. Use ^ for square root and only enter 1 number. use ** for exponents and enter the number, then the exponent
+	if (mathSymbol === "+") {
+		console.log(num1 + num2);
+	} else if (mathSymbol === "-") {
+		console.log(num1 - num2);
+	} else if (mathSymbol === "/") {
+		console.log(num1 / num2);
+	} else if (mathSymbol === "*") {
+		console.log(num1 * num2);
+	} else if (mathSymbol === "^" ) {
+		console.log(Math.sqrt(num1));
+	} else if (mathSymbol === "**") {
+		console.log(Math.pow(num1, num2));
+	} else if (mathSymbol === "%") {
+		console.log(num1 % num2);
+	}else {
+		console.log("Can't compute. Try again");
+	};
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
+
